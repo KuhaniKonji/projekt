a212cd96b49b3bde6600d03baccaa1427a5412d9
Slemensek6 <122811697+Slemensek6@users.noreply.github.com>
2024-10-08 08:54:16 +0200

    Update header.php

--- admin/deli_strani/header.php
+++ admin/deli_strani/header.php
@@ -1,8 +1,8 @@
-<span class="logo"><a href="index.php">Logo</a> </span>
+<span class="logo"><a href="index.php"><img src="../Slike/OIP.jpg" alt=""></a></span>
 <div class="nav">
   <span> <a href="predmeti.php">Predmeti</a> </span>
   <span> <a href="naloge.php">Naloge</a> </span>
   <span> <a href="ucenci.php">Učenci</a> </span>
   <span> <a href="ucitelji_zaposleni.php">Učitelji</a> </span>
 </div>
-<span class="odjava"><a href="php/odjava.php">Odjava</a> </span>
+<span class="profil"><span class="odjava"><a href="php/odjava.php"><img src="../Slike/logout-box-fill.svg" alt=""></a> </span></span>
