4895a70d5a989138261cdc71fbe9aaf0bcfafbf6
Slemensek6 <122811697+Slemensek6@users.noreply.github.com>
2024-11-04 23:18:21 +0100

    Update uciteljevi_podatki.php

--- ucenci/tabele/uciteljevi_podatki.php
+++ ucenci/tabele/uciteljevi_podatki.php
@@ -3,7 +3,6 @@
     <tr>
       <th>Ime učitelja</th>
       <th>Email</th>
-      <th>Telefon</th>
       <th>Št. predmetov</th>
     </tr>
   </thead>
@@ -19,7 +18,6 @@
     <tr>
       <td><a href="ucitelj.php?id_ucitelja=<?php echo $row_uciteljevi_podatki['id_ucitelja']; ?>"><?php echo $row_uciteljevi_podatki['ime_ucitelja'] . " " . $row_uciteljevi_podatki['priimek_ucitelja']; ?></a></td>
       <td><?php echo $row_uciteljevi_podatki['email']; ?></td>
-      <td><?php echo $row_uciteljevi_podatki['telefon']; ?></td>
       <td><?php echo $row_uciteljevi_podatki_count_predmeti_ucitelja['COUNT(id_predmeta)']; ?></td>
     </tr>
   <?php } ?>
